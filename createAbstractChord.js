48b9401aed03dd727fc7f266af50818aec222c69
emkoi <96228764+emkoi@users.noreply.github.com>
2019-07-13 12:56:05 -0700

    added basic functions for stochastic creation of chords & progressions

---
+++ createAbstractChord.js
@@ -0,0 +1,32 @@
+import {AbstractChord} from './abstractChord.js'
+
+// it may be worth creating a prototype object for configuration with 
+// the defaults, so users could see the fields without looking at the
+// implementation of this function
+export function createAbstractChord(configuration = {})
+{
+    let ret = new AbstractChord();
+    const DEFAULT_NUM_NOTES = 3;
+    
+
+    { // adding random notes
+        const numNotes = configuration.numNotes || DEFAULT_NUM_NOTES;
+        while (ret.notes.size < numNotes)
+        {
+            let newNote = Math.floor(Math.random() * 12);
+            ret = ret.addNote(newNote);
+        }
+    }
+
+    { // adding specific notes
+        if (configuration.mustContain)
+        {
+            let newRet = configuration.mustContain;
+            const numNotes = configuration.numNotes || DEFAULT_NUM_NOTES;
+            ret.notes.forEach((note) => if(newRet.size < numNotes) newRet.add(note));
+            ret = newRet;
+        }
+    }
+    
+    return ret;
+}
---
+++ createAbstractChordProgression.js
@@ -0,0 +1,21 @@
+import {createAbstractChord} from './createAbstractChord'
+
+// it may be worth creating a prototype object for configuration with 
+// the defaults, so users could see the fields without looking at the
+// implementation of this function
+export function createAbstractChordProgression(configuration = {})
+{
+    let ret = new Array;
+    
+    const DEFAULT_NUM_CHORDS = 3;
+    const numChords = configuration.numChords || DEFAULT_NUM_CHORDS;
+    
+    for (let i = 0; i < numChords; i++) 
+    {
+        // something similar to what a default config would look like
+        let chordConfig = {mustContain: new Set([0])};
+        ret.concat(createAbstractChord(chordConfig));
+    }
+    
+    return ret;
+}
